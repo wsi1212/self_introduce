8e7ab4e4d6a87eb5e2f70f4c32dd70fd0987fe5c
wsi1212 <wsiwsiwsi123@gmail.com>
2023-02-26 09:31:05 +0900

    Add: Local
    로컬 업로드

---
+++ MyPortfolio/app/src/main/java/com/example/myportfolio/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myportfolio
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.myportfolio.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
+
+        binding.btnInsta.setOnClickListener{
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/wsiwsiwsi123/"))
+            startActivity(browserIntent)
+        }
+
+        binding.tvInsta.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/wsiwsiwsi123/"))
+            startActivity(browserIntent)
+        }
+
+        binding.btnGithub.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com/wsi1212/"))
+            startActivity(browserIntent)
+        }
+
+        binding.tvGithub.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.github.com/wsi1212/"))
+            startActivity(browserIntent)
+        }
+
+
+
+    }
+}
